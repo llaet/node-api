ac1e6f6dd37aca835fc71c8fd935f671583db25f
Lucas Laet <lucas.laetlira@gmail.com>
2021-02-27 23:28:01 -0300

    Update,delete e find by ID endpoints added

--- src/UsuarioController.js
+++ src/UsuarioController.js
@@ -1,3 +1,7 @@
+/**
+ * Classe que define os endpoints da API.
+ */
+
 const express = require('express')
 const app = express()
 var usuarioService = require('./UsuarioService')
@@ -16,13 +20,21 @@ app.post('/users', (req, res, next) => {
 })
 
 app.delete('/users/:id', (req, res, next) => {
-
+    res.send(usuarioService.remover(parseInt(req.params.id)))
 })
 
 app.put('/users/:id', (req, res, next) => {
-
+    let usuario = {
+        nome: req.body.nome,
+        email: req.body.email,
+        endereco: req.body.endereco}
+    res.send(usuarioService.atualizarPorID(parseInt(req.params.id), usuario))
 })
 
 app.get('/users', (req, res, next) => {
     res.send(usuarioService.listarTodos())
+})
+
+app.get('/users/:id', (req, res, next) => {
+    res.send(usuarioService.buscaPorID(parseInt(req.params.id)))
 })
--- src/UsuarioService.js
+++ src/UsuarioService.js
@@ -1,3 +1,7 @@
+/**
+ * Classe que define as funcionalidades da API.
+ */
+
 var valorGerado = {
     _id: 0,
     get id() {
@@ -8,9 +12,37 @@ var valorGerado = {
 var _usuarios = []
 
 function salvar(novoUsuario){
+    //trata caso o usuário já exista
+    if (_usuarios.some(usuario => usuario.email == novoUsuario.email)) {
+        return _usuarios.filter(usuario => usuario.email == novoUsuario.email)
+    }
     novoUsuario.id = valorGerado.id
     _usuarios[novoUsuario.id] = novoUsuario
     return novoUsuario
+}
+
+function remover(usuarioID){
+    //verifica se o ID informado existe
+    if (_usuarios.some(usuario => usuario.id === usuarioID)) {
+        return delete _usuarios[usuarioID]
+    }
+    return 'Erro! Usuário não encontrado!'
+}
+
+function atualizarPorID(usuarioID, usuario){
+    usuario.id = usuarioID
+    if (_usuarios.some(objeto => objeto.id === usuarioID)) {
+        _usuarios[usuarioID] = usuario
+        return usuario
+    }
+    return 'Erro! Usuário não encontrado!'
+}
+
+function buscaPorID(usuarioID){
+    if (_usuarios.some(objeto => objeto.id === usuarioID)) {
+        return _usuarios[usuarioID]
+    }
+    return 'Erro! Usuário não encontrado!'
 }
 
 function listarTodos(){
