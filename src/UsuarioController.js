94fd5d5338e4b90d5ada1fbf4873b4d89563dd3b
Lucas Laet <lucas.laetlira@gmail.com>
2021-02-27 21:50:25 -0300

    Endpoints listar todos e salvar criados

--- src/UsuarioController.js
+++ src/UsuarioController.js
@@ -2,7 +2,26 @@ const express = require('express')
 const app = express()
 var usuarioService = require('./UsuarioService')
 
+app.use(express.json())
 app.listen(process.env.PORT || 3030)
+
+app.post('/users', (req, res, next) => {
+    let usuario = {
+        nome: req.body.nome,
+        email: req.body.email,
+        endereco: req.body.endereco}
+
+    usuario = usuarioService.salvar(usuario)
+    res.send(usuario)
+})
+
+app.delete('/users/:id', (req, res, next) => {
+
+})
+
+app.put('/users/:id', (req, res, next) => {
+
+})
 
 app.get('/users', (req, res, next) => {
     res.send(usuarioService.listarTodos())
--- src/UsuarioService.js
+++ src/UsuarioService.js
@@ -1,37 +1,20 @@
-const usuario = require('./Usuario')
+var valorGerado = {
+    _id: 0,
+    get id() {
+        return this._id++
+    }
+}
 
 var _usuarios = []
 
 function salvar(novoUsuario){
-    if (!(this._usuarios.some(objeto => objeto.id === novoUsuario.id))) {
-        this._usuarios[usuario.id] = usuario
-    }   
-}
-
-function remover(usuarioID){
-    try {
-        delete this._usuarios[usuarioID]
-    } catch (error) {
-        return 'Erro! Usuário não encontrado!'
-    }
-}
-
-function atualizarPorID(usuarioID, dadosAtualizados){
-    if (this._usuarios.some(objeto => objeto.id === novoUsuario.id)) {
-        this._usuarios[usuario.id] = dadosAtualizados
-    }
-    return 'Erro! Usuário não encontrado!'
-}
-
-function buscaPorID(usuarioID){
-    if (this._usuarios.some(objeto => objeto.id === novoUsuario.id)) {
-        return this._usuarios[usuario.id]
-    }
-    return 'Erro! Usuário não encontrado!'
+    novoUsuario.id = valorGerado.id
+    _usuarios[novoUsuario.id] = novoUsuario
+    return novoUsuario
 }
 
 function listarTodos(){
-    return this._usuarios
+    return _usuarios
 }
 
 module.exports = {salvar, remover, atualizarPorID, buscaPorID, listarTodos}
